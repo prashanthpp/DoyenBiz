8ddb9c7216be788922ca1c54220e30805c341abd
Prashanth Pogu <prpogu@microsoft.com>
2015-08-01 01:22:15 +0530

    adding MahApps

--- DoyenBiz.BankingKiosk/DoyenBiz.BankingKiosk/MainWindow.xaml.cs
+++ DoyenBiz.BankingKiosk/DoyenBiz.BankingKiosk/MainWindow.xaml.cs
@@ -12,13 +12,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace DoyenBiz.BankingKiosk
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow 
     {
         public MainWindow()
         {
