3b578b744a58ac28264f9c68589d10b9276832e3
Prashanth Pogu <prpogu@microsoft.com>
2015-08-15 00:53:18 +0530

    Main view model + Bank kiosk changes

---
+++ DoyenBiz.BankingKiosk/DoyenBiz.CitizenKiosk/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoyenBiz.CitizenKiosk.ViewModels
+{
+   public class MainViewModel : BaseViewModel
+    {
+        public int Balance;
+        public MainViewModel()
+        {
+            //Make get call and set Balance
+        }
+
+      
+    }
+}
